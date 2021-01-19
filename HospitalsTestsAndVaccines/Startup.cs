9cb34ac8850a09c58d1c3d99e424005923212434
Marios Michalopoulos <michalopoulos.m14@gmail.com>
2021-01-19 17:40:45 +0200

    Made minor changes

--- HospitalsTestsAndVaccines/Startup.cs
+++ HospitalsTestsAndVaccines/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HospitalsTestsAndVaccines.Migrations;
 using Microsoft.Owin;
 using Owin;
 
@@ -7,46 +8,11 @@ namespace HospitalsTestsAndVaccines
 {
     public partial class Startup
     {
-        public Startup(IConfiguration configuration)
+        public void Configuration(IAppBuilder app)
         {
-            Configuration = configuration;
-        }
+            ConfigureAuth(app);
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-            services.AddSignalR();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapHub<ChatHub>("/chathub");
-            });
+            app.MapSignalR();
         }
     }
 }
